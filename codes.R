51338a120fec39313bc8497815aff68b7eb25148
Wen <wenrg@users.noreply.github.com>
2017-06-24 15:07:30 -0400

    Update codes.R

--- codes.R
+++ codes.R
@@ -1,5 +1,4 @@
 # load data
-library(readr)
 df = read.csv('dirty_data.csv', header=T)
 
 #Populate the missing values in the Area variable with an appropriate values (Birmingham, Coventry, Dudley, Sandwell, Solihull, Walsall or Wolverhampton)
