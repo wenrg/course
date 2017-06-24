74026fbc542d5b17630bd2ee4136773ca18ae912
sding12 <caseyding@hotmail.com>
2017-06-24 15:52:18 -0400

    add comement

--- codes.R
+++ codes.R
@@ -7,8 +7,12 @@ random_area = sample(fill_area, nrow(df)-1, replace=T, prob=c(0.15, 0.1, 0.2, 0.
 df[2:nrow(df),'Area'] = random_area
 
 library("stringr")
+# remove special characters
 df$Street <- str_replace_all(df$Street,"[^[:alnum:]]", " ")
+# trim leading and trailing space
 df$Street <- str_replace_all(df$Street,"^\\s+|\\s+$", "")
+
+
 
 # Remove the “Strange HTML column”
 remove(list = df$`Strange HTML`)
