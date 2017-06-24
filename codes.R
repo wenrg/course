8b3b5c545174f7ec3010681b3a5b153a1b4e051b
Wen <wenrg@users.noreply.github.com>
2017-06-24 15:02:31 -0400

    Update codes.R

--- codes.R
+++ codes.R
@@ -3,7 +3,6 @@ library(readr)
 df = read.csv('dirty_data.csv', header=T)
 
 #Populate the missing values in the Area variable with an appropriate values (Birmingham, Coventry, Dudley, Sandwell, Solihull, Walsall or Wolverhampton)
-
 fill_area = c('Birmingham', 'Coventry', 'Dudley', 'Sandwell', 'Solihull', 'Walsall', 'Wolverhampton')
 random_area = sample(fill_area, nrow(df) - 1, replace=T, prob=c(0.15, 0.1, 0.2, 0.05, 0.12, 0.18, 0.2))
 df[2:nrow(df),'Area'] = random_area
