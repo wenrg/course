ebb5410f26ee352107f5658dbfd8c7bd3662afca
Wen LI <wen@mbp.local>
2017-06-24 14:54:53 -0400

    add codes

--- codes.R
+++ codes.R
@@ -1,2 +1,8 @@
 # load data
 df = read.csv('dirty_data.csv', header=T)
+
+fill_area = c('Birmingham', 'Coventry', 'Dudley', 'Sandwell', 'Solihull', 'Walsall', 'Wolverhampton')
+
+random_area = sample(fill_area, nrow(df) - 1, replace=T, prob=c(0.15, 0.1, 0.2, 0.05, 0.12, 0.18, 0.2))
+
+df[2:nrow(df),'Area'] = random_area
