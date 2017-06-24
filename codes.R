dfce9ac21b00f718d6885c83264d0ea3755dab5a
Wen <wenrg@users.noreply.github.com>
2017-06-24 15:58:15 -0400

    Update codes.R

--- codes.R
+++ codes.R
@@ -1,11 +1,16 @@
 # load data
 df <- read.csv('dirty_data.csv', header=T)
 
-#Populate the missing values in the Area variable with an appropriate values (Birmingham, Coventry, Dudley, Sandwell, Solihull, Walsall or Wolverhampton)
+# Populate the missing values in the Area variable with an appropriate values 
+# (Birmingham, Coventry, Dudley, Sandwell, Solihull, Walsall or Wolverhampton)
 fill_area = c('Birmingham', 'Coventry', 'Dudley', 'Sandwell', 'Solihull', 'Walsall', 'Wolverhampton')
 random_area = sample(fill_area, nrow(df)-1, replace=T, prob=c(0.15, 0.1, 0.2, 0.05, 0.12, 0.18, 0.2))
 df[2:nrow(df),'Area'] = random_area
 
+# Remove special characters, padding (the white space before and after the text) from Street 1 
+# and Street 2 variables. Make sure the first letters of street names are capitalized and the 
+# street denominations are following the same standard (for example, all streets are indicated 
+# as “str.”, avenues as “ave.”, etc.
 library("stringr")
 # remove special characters
 df$Street <- str_replace_all(df$Street,"[^[:alnum:]]", " ")
@@ -19,6 +24,8 @@ simpleCap = function(x){
         sep="", collapse=" ")
 }
 df$Street = sapply(df$Street, simpleCap)
+
+# If the value in Street 2 duplicates the value in Street 1, remove the value in Street 2
 
 
 
