735e8e9fc363223bdfe1978a6a1779c5e13613b5
sding12 <caseyding@hotmail.com>
2017-06-24 14:57:04 -0400

    Add code

--- codes.R
+++ codes.R
@@ -1,3 +1,11 @@
 # load data
+library(readr)
 df = read.csv('dirty_data.csv', header=T)
 #Populate the missing values in the Area variable with an appropriate values (Birmingham, Coventry, Dudley, Sandwell, Solihull, Walsall or Wolverhampton)
+
+
+
+
+# Remove the “Strange HTML column”
+remove(list = dirty_data$`Strange HTML`)
+
