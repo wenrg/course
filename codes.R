21dc8159e5fb0b86ff5de3f523aa57913ed9f52c
Wen <wenrg@users.noreply.github.com>
2017-06-24 15:30:10 -0400

    Update codes.R

--- codes.R
+++ codes.R
@@ -6,7 +6,8 @@ fill_area = c('Birmingham', 'Coventry', 'Dudley', 'Sandwell', 'Solihull', 'Walsa
 random_area = sample(fill_area, nrow(df)-1, replace=T, prob=c(0.15, 0.1, 0.2, 0.05, 0.12, 0.18, 0.2))
 df[2:nrow(df),'Area'] = random_area
 
-
+x <- "a1~!@#$%^&*(){}_+:\"<>?,./;'[]-=" #or whatever
+str_replace_all(x, "[[:punct:]]", " ")
 
 # Remove the “Strange HTML column”
 remove(list = df$`Strange HTML`)
