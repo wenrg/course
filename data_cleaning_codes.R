8f264392303d62eb45d96f8ad163b2099ab7ca97
RaeraeLi <li.rui.cathy@gmail.com>
2017-06-24 16:23:31 -0400

    Update data_cleaning_codes.R

--- data_cleaning_codes.R
+++ data_cleaning_codes.R
@@ -1,1 +1,8 @@
+# load data
+df = read.csv('dirty_data.csv', header=T)
 
+# Populate the missing values in the Area variable with an appropriate values 
+# (Birmingham, Coventry, Dudley, Sandwell, Solihull, Walsall or Wolverhampton)
+fill_area = c('Birmingham', 'Coventry', 'Dudley', 'Sandwell', 'Solihull', 'Walsall', 'Wolverhampton')
+random_area = sample(fill_area, nrow(df)-1, replace=T, prob=c(0.15, 0.1, 0.2, 0.05, 0.12, 0.18, 0.2))
+df[2:nrow(df),'Area'] = random_area
